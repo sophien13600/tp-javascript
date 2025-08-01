6f3eefd9881e912a3e5eba09c852ad45c910ea32
Sophien Bouderbala <sbdigitalagency@gmail.com>
2025-08-01 22:02:05 +0200

    need for speed ok

--- voiture.js
+++ voiture.js
@@ -1,18 +1,41 @@
+const voiture =document.getElementById('voiture')
+voiture.style['grid-column-end']=1
+voiture.style['grid-column-start']=1
+voiture.style['grid-row-start'] =1
+voiture.style['grid-row-end'] =1
+
 function moveRight(){
-    
-    const voiture =document.getElementById('voiture')
-    console.log(voiture.style);
-    voiture.style['grid-column-start'] =1
-    voiture.style['grid-column-end']=1
-    voiture.style['grid-row-end'] =1
-    voiture.style['grid-row-start']=1
-
-
-    voiture.style['grid-column-start'] ='   
-
-    voiture.style['grid-column-end']++
-
+    if((voiture.style['grid-row-start']>= 2) &&  (voiture.style['grid-row-start']<5) || (voiture.style['grid-row-start']>= 6) &&  (voiture.style['grid-row-start']<9)){
+    return
+    }
+    if((voiture.style['grid-column-end']) != 9){
+        voiture.style['grid-column-start']++
+        voiture.style['grid-column-end']++
+    }
 }
+function moveLeft(){
+    if((voiture.style['grid-row-start']>= 2) &&  (voiture.style['grid-row-start']<5) || (voiture.style['grid-row-start']>= 6) &&  (voiture.style['grid-row-start']<9)){
+    return
+    }
+        voiture.style['grid-column-start']--
+        voiture.style['grid-column-end']--
+   }
+function moveBottom(){
+    if((voiture.style['grid-column-start']>= 2) &&  (voiture.style['grid-column-start']<5) || (voiture.style['grid-column-start']>= 6) &&  (voiture.style['grid-column-start']<9)){
+    return
+    }
+    if((voiture.style['grid-row-end']) != 9){
+        voiture.style['grid-row-start']++
+        voiture.style['grid-row-end']++
+    }
+   }
+    function moveTop(){
+        if((voiture.style['grid-column-start']>= 2) &&  (voiture.style['grid-column-start']<5) ||(voiture.style['grid-column-start']>= 6) &&  (voiture.style['grid-column-start']<9)){
+    return
+    }
+        voiture.style['grid-row-start']--
+        voiture.style['grid-row-end']--
+   } 
 
 
 
